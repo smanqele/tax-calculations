b71a319b0a9c8d123825331697165b9915625b4b
Manqele, Sihle S <sihle.manqele@standardbank.co.za>
2021-06-12 16:17:30 +0200

    Tax Project beginning

---
+++ src/main/java/com/psiber/tax/PSiberTaxApplication.java
@@ -0,0 +1,14 @@
+package com.psiber.tax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PSiberTaxApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PSiberTaxApplication.class, args);
+  }
+
+
+}
---
+++ src/test/java/com/psiber/tax/PSiberTaxApplicationTest.java
@@ -0,0 +1,13 @@
+package com.psiber.tax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PSiberTaxApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
